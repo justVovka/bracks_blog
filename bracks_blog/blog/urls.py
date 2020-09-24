fa010b5a2018ca6be7bae388fe06eaa57a999dff
Vladimir Mokrov <vovkamokroff@gmail.com>
2020-09-24 22:31:01 +0300

    [task-0009] - Реализовал пагинацию на страницах: статьи по тегу и по категории, поправил соответствующие функции отображения во views.py, убрал черту в url

--- bracks_blog/blog/urls.py
+++ bracks_blog/blog/urls.py
@@ -6,8 +6,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('blog/category/<slug:category_link>/', views.articles_by_category, name='articles_by_category'),
-    path('blog/tag/<slug:tag_link>/', views.articles_by_tag, name='articles_by_tag'),
+    path('blog/category/<slug:category_link>', views.articles_by_category, name='articles_by_category'),
+    path('blog/tag/<slug:tag_link>', views.articles_by_tag, name='articles_by_tag'),
     path('blog/<slug:article_link>/', views.single_article, name='article'),
     path('about/', views.about, name='about'),
 ]
