2d04efcd83f837a980b563d33ea202648ea4b1da
Vladimir Mokrov <vovkamokroff@gmail.com>
2020-09-30 21:35:44 +0300

    [task-0011] - Создал страницы ошибки 404 и 500

--- bracks_blog/blog/views.py
+++ bracks_blog/blog/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404
 
 from .models import Article, Category, Tag
 
